d57589fb6be20a99d9bdde121b4d37064c39f13c
mingtian20 <2572182545@qq.com>
2026-04-03 12:17:49 +0800

    平台属性-规格参数查询功能_shibh

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
@@ -6,11 +6,7 @@ import java.util.Map;
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import com.atguigu.gulimall.product.vo.AttrVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.product.entity.AttrEntity;
 import com.atguigu.gulimall.product.service.AttrService;
@@ -88,4 +84,9 @@ public class AttrController {
         return R.ok();
     }
 
+    @GetMapping("/base/list/{catelogId}")
+    public R baseAttrList(@RequestParam Map<String, Object> params,@PathVariable("catelogId")Long catelogId){
+        PageUtils pageUtils = attrService.queryBaseAttrPage(params,catelogId);
+        return R.ok().put("page", pageUtils);
+    }
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,12 +1,23 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.atguigu.gulimall.product.dao.AttrGroupDao;
+import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.atguigu.gulimall.product.entity.AttrGroupEntity;
+import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.AttrAttrgroupRelationService;
+import com.atguigu.gulimall.product.vo.AttrRespVO;
 import com.atguigu.gulimall.product.vo.AttrVO;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,6 +34,17 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
 
     @Autowired
     private  AttrAttrgroupRelationService attrAttrgroupRelationService;
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @Autowired
+    private AttrAttrgroupRelationDao   attrAttrgroupRelationDao;
+
+    @Autowired
+    private AttrGroupDao   attrGroupDao;
+
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -44,6 +66,49 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         relationEntity.setAttrId(attrEntity.getAttrId());
         attrAttrgroupRelationService.save(relationEntity);
 
+    }
+
+
+    @Override
+    public PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId) {
+        QueryWrapper<AttrEntity> queryWrapper = new QueryWrapper<>();
+        if (catelogId != 0 ) {
+            queryWrapper.eq("catelog_id",catelogId);
+        }
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)){
+            queryWrapper.and((wrapper)->{ wrapper.eq("attr_id",key).or().like("attr_name",key);});
+        }
+
+        IPage<AttrEntity> page = this.page(
+                new Query<AttrEntity>().getPage(params),
+                queryWrapper
+        );
+        List<AttrRespVO> attrRespVOList = page.getRecords().stream().map((attrEntity) -> {
+            AttrRespVO attrRespVO = new AttrRespVO();
+            BeanUtils.copyProperties(attrEntity, attrRespVO);
+
+            //赋值catelog_name
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getCatelogId());
+            if (categoryEntity != null) {
+                attrRespVO.setCatelogName(categoryEntity.getName());
+            }
+
+            //赋值groupName
+            AttrAttrgroupRelationEntity relationEntity = attrAttrgroupRelationDao
+                    .selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrEntity.getAttrId()));
+            if (relationEntity != null) {
+                attrRespVO.setAttrGroupId(relationEntity.getAttrGroupId());
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(relationEntity.getAttrGroupId());
+                attrRespVO.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+
+            return attrRespVO;
+        }).collect(Collectors.toList());
+
+        PageUtils pageUtils = new PageUtils(page);
+        pageUtils.setList(attrRespVOList);
+        return pageUtils;
     }
 
 }
